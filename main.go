65b1370ae7e5156df21c548f6c355720810e0caa
Nikola Stankovic <nikola@stankovic.xyz>
2019-08-05 20:00:03 +0200

    add file flag and check command

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ var app = cli.NewApp()
 func main() {
 	info()
 	commands()
+	flags()
 
 	err := app.Run(os.Args)
 	if err != nil {
@@ -22,7 +23,7 @@ func main() {
 
 func info() {
 	app.Name = "Vleas CLI"
-	app.Usage = "Vleas is a open source CLI for updating Gradle and Maven dependencies."
+	app.Usage = "Vleas is an easy to use open source CLI for maintaining dependencies."
 	app.Version = "0.0.1"
 	app.Compiled = time.Now()
 	app.Authors = []cli.Author{
@@ -38,14 +39,36 @@ func commands() {
 		{
 			Name:    "update",
 			Aliases: []string{"u"},
-			Usage:   "Update dependencies",
+			Usage:   "Update all dependencies to newest version",
 			Action: func(c *cli.Context) {
 				update()
+			},
+		},
+		{
+			Name:    "check",
+			Aliases: []string{"c"},
+			Usage:   "Check for new dependency updates",
+			Action: func(c *cli.Context) {
+				check()
 			},
 		},
 	}
 }
 
+func flags()  {
+	app.Flags = []cli.Flag {
+		cli.StringFlag{
+			Name: "file, f",
+			Usage: "Path to dependencies file `FILE`",
+			Required: true,
+		},
+	}
+}
+
 func update()  {
-	fmt.Println("update")
+	fmt.Println("update dependencies")
+}
+
+func check()  {
+	fmt.Println("check dependencies")
 }
