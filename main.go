b10d61e44997243038d98b79a315ecf6287208dc
Nikola Stankovic <nikola@stankovic.xyz>
2019-08-07 03:50:39 +0200

    implement check command

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/urfave/cli"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
+	"strings"
 	"time"
+
+	. "github.com/logrusorgru/aurora"
 )
 
 var app = cli.NewApp()
 
+var filePath string
+
 func main() {
-	searchOnMavenCentral()
+	//var groupId = "ch.viascom.groundwork"
+	//var artifactId = "foxhttp"
+	//searchOnMavenCentral(groupId, artifactId)
 
 	info()
 	commands()
@@ -56,6 +62,14 @@ func commands() {
 				check()
 			},
 		},
+		{
+			Name:    "search",
+			Aliases: []string{"s"},
+			Usage:   "Search for a dependency",
+			Action: func(c *cli.Context) {
+				search()
+			},
+		},
 	}
 }
 
@@ -64,7 +78,7 @@ func flags()  {
 		cli.StringFlag{
 			Name: "file, f",
 			Usage: "Path to dependencies file `FILE`",
-			Required: true,
+			Destination: &filePath,
 		},
 	}
 }
@@ -74,29 +88,70 @@ func update()  {
 }
 
 func check()  {
-	fmt.Println("check dependencies")
-}
+	var totalDeps int
+	configurations := []string{"api", "implementation", "compileOnly", "runtimeOnly", "testImplementation", "testCompileOnly", "testRuntimeOnly", "compile", "runtime"}
 
-func searchOnMavenCentral() {
-	var groupId = "ch.viascom.groundwork"
-	var artifactId = "foxhttp"
-
-	qp := "q=g:\"" + groupId + `"+AND+a:"` + artifactId + `"` +
-		"&rows=20" +
-		"&core=gav"
-
-	u := &url.URL{
-		Scheme:   "http",
-		Host:     "search.maven.org",
-		Path:     "/solrsearch/select",
-		RawQuery: qp,
-	}
-
-	resp, err := http.Get (u.String())
+	file, err := os.Open(filePath)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		for _, configuration := range configurations {
+			if strings.Contains(scanner.Text(), configuration + " ") {
+
+				totalDeps++
+
+				dep := scanner.Text()
+				dep = strings.Replace(dep, configuration, "", 1)
+				dep = strings.ReplaceAll(dep, "\"", "")
+				dep = strings.ReplaceAll(dep, "'", "")
+				dep = strings.TrimSpace(dep)
+
+				dependencies := strings.Split(dep, ":")
+
+				if len(dependencies) == 2 {
+					totalDeps--
+					continue
+				}
+
+				if len(dependencies) >= 1 {
+					fmt.Print("group: ", Yellow(dependencies[0]), ", ")
+
+					if len(dependencies) >= 2 {
+						fmt.Print("name: ", Yellow(dependencies[1]), ", ")
+
+						if len(dependencies) >= 3 {
+							fmt.Print("version: ", Yellow(dependencies[2]),  " --> ")
+
+							fmt.Println(Red("outdated"))
+						}
+					}
+				}
+			}
+		}
 	}
 
-	log.Println(resp.Request.URL)
-	log.Println(resp)
+	fmt.Println("\nTotal dependencies found:", totalDeps)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
+
+func search()  {
+	fmt.Println("search dependency")
+}
+
+//func searchOnMavenCentral(groupId, artifactId string) {
+//	var url = "http://search.maven.org/solrsearch/select?q=g:%22GROUP_ID%22+AND+a:%22ARTIFACT_ID%22&rows=20&core=gav"
+//	url = strings.Replace(url,"GROUP_ID", groupId, -1)
+//	url = strings.Replace(url,"ARTIFACT_ID", artifactId, -1)
+//
+//	resp, err := http.Get (url)
+//	if err != nil {
+//		panic(err.Error())
+//	}
+//}
