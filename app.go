fd3f70cf5fd848c43398aa4703dd101ca19cba16
Nikola Stankovic <nikola@stankovic.xyz>
2019-10-04 18:01:09 +0200

    working state

--- app.go
+++ app.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/tidwall/gjson"
 	"github.com/urfave/cli"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"regexp"
 	"sort"
+	"strings"
 )
 
 func main() {
@@ -17,9 +22,9 @@ func main() {
 	app.Email = "nikola@stankovic.xyz"
 	app.Description = "Vleas is an easy to use open source CLI for maintaining dependencies."
 
-	app.Flags = []cli.Flag {
+	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name: "file, f",
+			Name:  "file, f",
 			Usage: "Load dependencies from `FILE`",
 		},
 	}
@@ -29,7 +34,7 @@ func main() {
 			Name:    "check",
 			Aliases: []string{"c"},
 			Usage:   "check for new dependencies",
-			Action:  func(c *cli.Context) error {
+			Action: func(c *cli.Context) error {
 				check(c.GlobalString("file"))
 				return nil
 			},
@@ -38,7 +43,7 @@ func main() {
 			Name:    "update",
 			Aliases: []string{"u"},
 			Usage:   "update all dependencies to latest version",
-			Action:  func(c *cli.Context) error {
+			Action: func(c *cli.Context) error {
 				update(c.GlobalString("file"))
 				return nil
 			},
@@ -54,10 +59,40 @@ func main() {
 	}
 }
 
-func check(file string)  {
-	fmt.Println("check dependencies from file: " + file)
+func check(file string) {
+	contentBytes, _ := ioutil.ReadFile(file)
+	content := string(contentBytes)
+
+	regex := regexp.MustCompile("(?P<group>[^\"$]+):(?P<name>[^\"$]+):(?P<version>[^\"$]+)")
+	deps := regex.FindAllStringSubmatch(content, -1)
+	for i := range deps {
+		group := deps[i][1]
+		name := deps[i][2]
+		newVersion :=newDepVersion(group, name)
+		deps[i] = append(deps[i], newVersion)
+
+		fmt.Printf("group: %s name: %s version: %s --> %s\n", deps[i][1], deps[i][2], deps[i][3], deps[i][4])
+	}
 }
 
-func update(file string)  {
+func update(file string) {
 	fmt.Println("update dependencies from file: " + file)
+}
+
+func newDepVersion(group, name string) string {
+	url := "http://search.maven.org/solrsearch/select?q=g:%22#GROUP%22+AND+a:%22#NAME%22&rows=20&core=gav"
+	url = strings.Replace(url, "#GROUP", group, 1)
+	url = strings.Replace(url, "#NAME", name, 1)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return gjson.Get(string(body), "response.docs.1.v").String()
 }
