025991101f8dfc9f163a031ab59cef2f6cb7e888
Nikola Stankovic <nikola@stankovic.xyz>
2019-10-05 04:16:22 +0200

    save point

--- app.go
+++ app.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/rhea-0b1/vleas/gradle"
 	"github.com/rhea-0b1/vleas/model"
-	"github.com/rhea-0b1/vleas/util"
-	"github.com/tidwall/gjson"
 	"github.com/urfave/cli"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"regexp"
 	"sort"
-	"strings"
 )
 
 var resolvedDependencies = make([]model.Dependency, 0)
@@ -65,38 +60,18 @@ func main() {
 }
 
 func check(file string) {
-	contentBytes, _ := ioutil.ReadFile(file)
-	content := string(contentBytes)
-
-	regex := regexp.MustCompile("(?P<group>[^\"$,;()\\[\\]{}']+):(?P<name>[^\"$,;()\\[\\]{}']+):(?P<version>[^\"$,;()\\[\\]{}']+)")
-	deps := regex.FindAllStringSubmatch(content, -1)
-
-	for i := range deps {
-		group := deps[i][1]
-		name := deps[i][2]
-		latestVersion := fetchLatestDeps(group, name)
-
-		deps[i] = append(deps[i], latestVersion)
-
-		if validDep(deps[i]) {
-			resolvedDependencies = append(resolvedDependencies, model.Dependency{
-				Group:          deps[i][1],
-				Name:           deps[i][2],
-				CurrentVersion: deps[i][3],
-				LatestVersion:  deps[i][4],
-			})
-		} else {
-			if strings.EqualFold(deps[i][3], deps[i][4]) == false {
-				unresolvedDependencies = append(unresolvedDependencies, model.Dependency{
-					Group:          deps[i][1],
-					Name:           deps[i][2],
-					CurrentVersion: deps[i][3],
-					LatestVersion:  deps[i][4],
-				})
-			}
-		}
+	buildTool := "Gradle"
+	switch buildTool {
+	case "Gradle":
+		gradle.Handle(file)
 	}
-	resolvedDependencies = util.RemoveDuplicates(resolvedDependencies)
+
+	printResult()
+}
+
+func printResult() {
+	resolvedDependencies = gradle.ResolvedDependencies
+	unresolvedDependencies = gradle.UnresolvedDependencies
 
 	if len(resolvedDependencies) > 0 {
 		fmt.Printf("\nVleas found %d dependency update(s):\n\n", len(resolvedDependencies))
@@ -116,39 +91,6 @@ func check(file string) {
 	}
 }
 
-func validDep(dep []string) bool {
-	currentVersion := dep[3]
-	latestVersion := dep[4]
-
-	if strings.EqualFold(currentVersion, latestVersion) {
-		return false
-	}
-
-	if latestVersion == "" {
-		return false
-	}
-
-	return true
-}
-
 func update(file string) {
 	fmt.Println("update deps from file: " + file)
-}
-
-func fetchLatestDeps(group, name string) string {
-	url := "http://search.maven.org/solrsearch/select?q=g:%22#GROUP%22+AND+a:%22#NAME%22&"
-	url = strings.Replace(url, "#GROUP", group, 1)
-	url = strings.Replace(url, "#NAME", name, 1)
-
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	return gjson.Get(string(body), "response.docs.0.latestVersion").String()
 }
